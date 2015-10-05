6c1628be9ea0d410aaddf717105dde3ce19aa726
Noxer <tim@plusmid.net>
2015-10-05 14:50:15 +0200

    Minor changes

--- main.go
+++ main.go
@@ -21,7 +21,6 @@ func init() {
 	if err != nil {
 		fmt.Errorf("Error: %s\n", err)
 	}
-
 	flag.StringVar(&dir, "d", d, "The directory that should be served.")
 	flag.StringVar(&port, "p", ":8888", "The port/interface the server should work on.")
 	flag.StringVar(&crtFile, "c", "", "The certificate file for HTTPS connections.")
@@ -30,21 +29,17 @@ func init() {
 }
 
 func main() {
-	fmt.Println("Starting nano server...")
-	fmt.Printf("Serving directory: %s\n", dir)
-
+	fmt.Printf("Starting nano server...\nServing directory: %s\n", dir)
 	if crtFile != "" && keyFile != "" {
 		fmt.Printf("Using certificate: %s\nUsing private key: %s\n", crtFile, keyFile)
 	} else {
 		fmt.Println("No certificate and/or key provided!")
 	}
-
 	srv := &http.Server{
 		Addr:    port,
 		Handler: &requestLogger{http.FileServer(http.Dir(dir))},
 	}
 	http2.ConfigureServer(srv, nil)
-
 	if crtFile != "" && keyFile != "" {
 		fmt.Printf("Starting HTTPS server on port %s... Kill with [Ctrl]-[C]!\n", port)
 		if err := srv.ListenAndServeTLS(crtFile, keyFile); err != nil {
